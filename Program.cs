2ae6aa3e0a0d39d32b7ee38a8ba036b2aca7c028
Pedro Henrique <excaliburpedrohenrique@email.com>
2026-05-19 20:37:07 -0300

    master

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Digite o primeiro numero: ");
+double num1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o segundo numero: ");
+
+double num2 = double.Parse(Console.ReadLine());
+
+double Soma = (num1 + num2);
+double Subtração = (num1 - num2);
+double Divisão = (num1 / num2);
+double Multiplicação = (num1 * num2);
+double Resto_divisão = (num1 % num2);
+
+Console.WriteLine("O resultado soma é: " + Soma);
+Console.WriteLine("O resultado da subtação é: " + Subtração);
+Console.WriteLine("O resultado da divisãoé: " + Divisão);
+Console.WriteLine("O resultado da multiplicação é: " + Multiplicação);
+Console.WriteLine("O resto é: " + Resto_divisão);
